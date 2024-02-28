59ade9f32e8565ab1118a9ae4ac0802a4f488e5b
jpal91 <pallanschja@gmail.com>
2024-02-28 10:22:58 -0500

    finished luhn, added luhn-from

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,4 +1,26 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    todo!("Is the Luhn checksum for {code} valid?");
+    if code.trim().len() <= 1 {
+        return false;
+    }
+
+    let mut nums: Vec<u32> = vec![];
+    let code = code.chars().filter(|c| c != &' ').rev().enumerate();
+
+    for (i, c) in code {
+        if !c.is_ascii_digit() {
+            return false;
+        };
+
+        let num = match c.to_digit(10) {
+            Some(n) if i % 2 != 0 && n * 2 > 9 => (n * 2) - 9,
+            Some(n) if i % 2 != 0 => n * 2,
+            Some(n) => n,
+            None => return false,
+        };
+
+        nums.push(num)
+    }
+
+    nums.into_iter().sum::<u32>() % 10 == 0
 }
