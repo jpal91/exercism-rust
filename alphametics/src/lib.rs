7dddbc6a23171dbfe16d0faa784448f204fac112
jpal91 <pallanschja@gmail.com>
2024-02-28 09:32:38 -0500

    switching to different excercise

--- alphametics/src/lib.rs
+++ alphametics/src/lib.rs
@@ -1,5 +1,38 @@
+
+#![allow(unused)]
 use std::collections::HashMap;
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
-    todo!("Solve the alphametic {input:?}")
+    let mut split_eq = input.split("==");
+
+    if split_eq.clone().count() != 2 {
+        return None;
+    };
+
+    let operand = parse_operand(split_eq.next().unwrap());
+    let value = str_to_u32(split_eq.next().unwrap());
+    todo!()
 }
+
+fn parse_operand(input: &str) -> u32 {
+    let it = input.split("+");
+    let mut total = 0;
+
+    for v in it {
+        let num = str_to_u32(v);
+        total += num
+    };
+
+    total
+}
+
+fn str_to_u32(input: &str) -> u32 {
+    let val: String = input
+    .trim()
+    .as_bytes()
+    .iter()
+    .map(|&x| char::from_u32((x - 65) as u32).unwrap())
+    .collect();
+
+    val.parse().unwrap()
+} 
