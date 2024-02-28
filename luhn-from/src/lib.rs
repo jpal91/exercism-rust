80ca1634c9ae325b58f626982d97844e18636abf
jpal91 <pallanschja@gmail.com>
2024-02-28 11:03:45 -0500

    finished luhn-from, moving to luhn-trait

--- luhn-from/src/lib.rs
+++ luhn-from/src/lib.rs
@@ -1,8 +1,8 @@
-pub struct Luhn;
+pub struct Luhn(bool);
 
 impl Luhn {
     pub fn is_valid(&self) -> bool {
-        todo!("Determine if the current Luhn struct contains a valid credit card number.");
+        self.0
     }
 }
 
@@ -11,8 +11,45 @@ impl Luhn {
 /// by hand for the every other type presented in the test suite,
 /// but your solution will fail if a new type is presented.
 /// Perhaps there exists a better solution for this problem?
-impl<'a> From<&'a str> for Luhn {
-    fn from(input: &'a str) -> Self {
-        todo!("From the given input '{input}' create a new Luhn struct.");
+
+impl<T> From<T> for Luhn 
+where
+    T: ToString
+{
+    fn from(input: T) -> Self {
+        let input = input.to_string();
+
+        if input.trim().len() <= 1 {
+            return Luhn(false)
+        }
+
+        if let Some(sum) = luhn_strs(&input) {
+            Luhn(sum % 10 == 0)
+        } else {
+            Luhn(false)
+        }
     }
+}
+
+
+fn luhn_strs(input: &str) -> Option<u32> {
+    let mut nums: Vec<u32> = vec![];
+    let code = input.chars().filter(|c| c != &' ').rev().enumerate();
+
+    for (i, c) in code {
+        if !c.is_ascii_digit() {
+            return None;
+        };
+
+        let num = match c.to_digit(10) {
+            Some(n) if i % 2 != 0 && n * 2 > 9 => (n * 2) - 9,
+            Some(n) if i % 2 != 0 => n * 2,
+            Some(n) => n,
+            None => return None,
+        };
+
+        nums.push(num)
+    }
+
+    Some(nums.into_iter().sum::<u32>())
 }
