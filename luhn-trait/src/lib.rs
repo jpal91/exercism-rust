f9c32792653ed30614be3945d7ca0b4c50cf53a1
jpal91 <pallanschja@gmail.com>
2024-02-28 11:10:02 -0500

    finished luhn-trait

--- luhn-trait/src/lib.rs
+++ luhn-trait/src/lib.rs
@@ -7,8 +7,35 @@ pub trait Luhn {
 /// by hand for the every other type presented in the test suite,
 /// but your solution will fail if a new type is presented.
 /// Perhaps there exists a better solution for this problem?
-impl<'a> Luhn for &'a str {
+impl<T> Luhn for T 
+where
+    T: ToString
+{
     fn valid_luhn(&self) -> bool {
-        todo!("Determine if '{self}' is a valid credit card number.");
+        let input = self.to_string();
+
+        if input.trim().len() <= 1 {
+            return false
+        }
+
+        let mut nums: Vec<u32> = vec![];
+        let code = input.chars().filter(|c| c != &' ').rev().enumerate();
+    
+        for (i, c) in code {
+            if !c.is_ascii_digit() {
+                return false;
+            };
+    
+            let num = match c.to_digit(10) {
+                Some(n) if i % 2 != 0 && n * 2 > 9 => (n * 2) - 9,
+                Some(n) if i % 2 != 0 => n * 2,
+                Some(n) => n,
+                None => return false,
+            };
+    
+            nums.push(num)
+        }
+    
+        nums.into_iter().sum::<u32>() % 10 == 0
     }
 }
