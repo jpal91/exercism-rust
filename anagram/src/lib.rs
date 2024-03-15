a539591e7fbf433a8f14a9c2bfdd6d9b22d846f8
jpal91 <pallanschja@gmail.com>
2024-03-15 08:50:20 -0400

    finished anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,5 +1,37 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+
+#[derive(PartialEq)]
+struct Counter(HashMap<char, u32>);
+
+struct Word<'a>(&'a str, Counter);
+
+impl<'a> Word<'a> {
+    fn new(word: &'a str) -> Self {
+        Word(word, Counter::new(word))
+    }
+
+    fn is_ana(&self, other: &Word) -> Option<&'a str> {
+        (other.0.to_lowercase() != self.0.to_lowercase() && other.1 == self.1).then_some(self.0)
+    }
+}
+
+impl Counter {
+    fn new(word: &str) -> Self {
+        let mut map: HashMap<char, u32> = HashMap::new();
+
+        word.to_lowercase().chars().for_each(|c| {
+            map.entry(c).and_modify(|n| *n += 1).or_default();
+        });
+
+        Self(map)
+    }
+}
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+    let cmp_word = Word::new(word);
+
+    possible_anagrams
+        .iter()
+        .filter_map(|&w| Word::new(w).is_ana(&cmp_word))
+        .collect()
 }
