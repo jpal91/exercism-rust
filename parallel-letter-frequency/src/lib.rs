4de8d98a1d9c9165f74290e8da1de1148bce2d58
jpal91 <pallanschja@gmail.com>
2024-03-17 21:45:20 -0400

    finished parallel-letter-freq

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,12 +1,38 @@
 use std::collections::HashMap;
+use std::thread;
 
+fn counter(inp: &[&str]) -> HashMap<char, usize> {
+    let mut map = HashMap::new();
+
+    inp.iter()
+        .flat_map(|l| {
+            l.chars()
+                .filter_map(|c| c.is_alphabetic().then_some(c.to_ascii_lowercase()))
+        })
+        .for_each(|c| {
+            map.entry(c).and_modify(|v| *v += 1).or_insert(1);
+        });
+
+    map
+}
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    todo!(
-        "Count the frequency of letters in the given input '{input:?}'. Ensure that you are using {} to process the input.",
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{worker_count} workers"),
-        }
-    );
+    match input.len() {
+        0 => HashMap::new(),
+        n if n < 100 => counter(input),
+        n => thread::scope(|s| {
+            let chunks = input.chunks(n / worker_count + 1);
+            let handles = chunks.map(|c| s.spawn(|| counter(c))).collect::<Vec<_>>();
+            let mut map = HashMap::new();
+
+            handles.into_iter().for_each(|h| {
+                h.join()
+                    .unwrap()
+                    .into_iter()
+                    .for_each(|(k, v)| (*map.entry(k).or_default()) += v)
+            });
+
+            map
+        }),
+    }
 }
